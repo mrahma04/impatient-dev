0e0939139d85065f6a2e99c9838e94fc8efb5f31
Mohammad Rahman <mbrahman@gmail.com>
2022-07-01 07:12:24 -0400

    testing complete

--- main.js
+++ main.js
@@ -1,13 +1,51 @@
-require('dotenv').config()
+require("dotenv").config();
 
 const accountSid = process.env.TWILIO_ACCOUNT_SID;
 const authToken = process.env.TWILIO_AUTH_TOKEN;
-const client = require("twilio")(accountSid, authToken);
+const twilioClient = require("twilio")(accountSid, authToken);
 
-client.messages
-  .create({
-    body: "Build complete!",
-    from: process.env.TWILIO_SRC_TEL,
-    to: process.env.TWILIO_DST_TEL,
-  })
-  .then((message) => console.log(message.sid));
+const { PubSub } = require("@google-cloud/pubsub");
+
+// Instantiates a new PubSub client
+const pubSubClient = new PubSub();
+
+const subscriptionNameOrId = "buildComplete-sub";
+const timeout = 60 * 60;
+
+function listenForMessage() {
+  // References an existing subscription
+  const subscription = pubSubClient.subscription(subscriptionNameOrId);
+
+  // Create an event handler to handle messages
+  let messageCount = 0;
+  const messageHandler = (message) => {
+    console.log(`Received message ${message.id}`);
+    console.log(`\tData: ${message.data}`);
+    console.log(`\tAttributes: ${message.attributes}`);
+    messageCount += 1;
+
+    // "Ack" (acknowledge receipt of) the message
+    message.ack();
+
+    // send message using Twilio client
+    twilioClient.messages
+      .create({
+        body: "Build complete! From Impatient Dev",
+        from: process.env.TWILIO_SRC_TEL,
+        to: process.env.TWILIO_DST_TEL,
+      })
+      .then((message) => console.log(message.sid));
+  };
+
+  // Listen for new messages until timeout is hit
+  subscription.on("message", messageHandler);
+
+  // Comment this out if for continuous pull
+  //   setTimeout(() => {
+  //   // Turns off the subscription listener
+  //     subscription.removeListener("message", messageHandler);
+  //     console.log(`${messageCount} message(s) received`);
+  //   }, timeout * 1000);
+}
+
+listenForMessage();
