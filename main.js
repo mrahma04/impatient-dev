e7c0f61b4a2dd7af87d4730f4e0eb1619f73ca39
Mohammad Rahman <mbrahman@gmail.com>
2022-07-01 05:55:12 -0400

    initial commit

---
+++ main.js
@@ -0,0 +1,13 @@
+require('dotenv').config()
+
+const accountSid = process.env.TWILIO_ACCOUNT_SID;
+const authToken = process.env.TWILIO_AUTH_TOKEN;
+const client = require("twilio")(accountSid, authToken);
+
+client.messages
+  .create({
+    body: "Build complete!",
+    from: process.env.TWILIO_SRC_TEL,
+    to: process.env.TWILIO_DST_TEL,
+  })
+  .then((message) => console.log(message.sid));
